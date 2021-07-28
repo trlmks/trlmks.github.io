f443e257183e7778c465eeb5a797cd1672d91dc1
trlmks <mbelganteng@gmail.com>
2021-07-28 17:07:03 +0700

    penghapusan scipt js

--- script.js
+++
@@ -1,6 +0,0 @@
-const toggle=document.querySelector('.menu-toggle input');
-const ul = document.querySelector('nav ul');
-
-toggle.addEventListener('click',function(){
-    ul.classList.toggle('slide');
-})
