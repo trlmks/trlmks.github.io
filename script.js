f6c6c0f297450110c468ad3a8fb4950f3fb2f3b4
trlmks <mbelganteng@gmail.com>
2021-07-28 19:31:47 +0700

    pelengkap

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbz3ai5LwbcW1gl04iKD0WLktkUgFLNlYNmFxgoWkNN713d6uIOioSDfh3R9nhPfo8GewA/exec'
+    const form = document.forms['submit-to-google-sheet']
+  
+    form.addEventListener('submit', e => {
+      e.preventDefault()
+      fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+        .then(response => {
+            alert('terima kasih, pesan anda sudah terkirim')
+            form.reset();
+            console.log('Success!', response)})
+        .catch(error => console.error('Error!', error.message))
+    })
+//============================= 
 const toggle=document.querySelector('.menu-toggle input');
 const ul = document.querySelector('nav ul');
 
 toggle.addEventListener('click',function(){
     ul.classList.toggle('slide');
 })
-// say hello
